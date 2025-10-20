2488210d7e42f61154c9f8a25da485f3db03a713
Paulo Victor <paulo.andrade@planejamento.rj.gov.br>
2025-10-20 13:45:27 -0300

    Projeto criado

---
+++ src/main/java/com/paulo/mongoWorkshop/MongoWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.mongoWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWorkshopApplication.class, args);
+	}
+
+}
