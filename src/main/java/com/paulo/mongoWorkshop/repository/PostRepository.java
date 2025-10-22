305462dfbb69746ecd77d7fac4aff5399de4c846
Paulo Victor <pvictorma@gmail.com>
2025-10-22 18:07:36 -0300

    Post

---
+++ src/main/java/com/paulo/mongoWorkshop/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.paulo.mongoWorkshop.repository;
+
+import com.paulo.mongoWorkshop.domain.Post;
+import com.paulo.mongoWorkshop.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
